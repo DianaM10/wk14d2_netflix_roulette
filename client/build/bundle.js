a8ed3a130267a1c81e7a0259ef0a31c49c9f7e6d
Diana Man <dianaman82@gmail.com>
2017-01-10 14:56:08 +0000

    can search api

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -19772,6 +19772,16 @@
 	    this.setState({
 	      currentSearch: searchText
 	    });
+	    if (searchText.length > 5) {
+	      var url = 'http://netflixroulette.net/api/api.php?actor=' + encodeURI(searchText);
+	      var request = new XMLHttpRequest();
+	      request.open('GET', url);
+	      request.onload = function () {
+	        var data = JSON.parse(request.responseText);
+	        console.log("data: ", data);
+	      };
+	      request.send();
+	    }
 	  },
 	
 	  render: function render() {
--- client/src/containers/ShowContainer.jsx
+++ client/src/containers/ShowContainer.jsx
@@ -15,6 +15,17 @@ var ShowContainer = React.createClass({
     this.setState({
       currentSearch: searchText
     });
+    if( searchText.length > 5) {
+      var url = 'http://netflixroulette.net/api/api.php?actor=' + encodeURI(searchText);
+      var request = new XMLHttpRequest();
+      request.open('GET', url);
+      request.onload = function() {
+        var data = JSON.parse(request.responseText);
+        console.log("data: ", data);
+
+      }
+      request.send();
+    }
   },
 
   render: function() {
